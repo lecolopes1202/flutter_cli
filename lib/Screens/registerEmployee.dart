c2e841668fb277769bf1b9c7bca521dece4e58e9
lecolopes1202 <alexlopes1202@gmail.com>
2021-07-27 22:29:24 -0300

    54

--- lib/Screens/registerEmployee.dart
+++ lib/Screens/registerEmployee.dart
@@ -14,7 +14,7 @@ class registerEmployee extends StatefulWidget {
 
 Future<EmployeeModel> registerEmployees(
     String firstName, String lastName, BuildContext context) async {
-  var Url = "http://localhost:8080/addemployee";
+  var Url = "http://10.0.2.2:8080/addemployee";
   var response = await http.post(Url,
       headers: <String, String>{
         'Content-Type': 'application/json',
